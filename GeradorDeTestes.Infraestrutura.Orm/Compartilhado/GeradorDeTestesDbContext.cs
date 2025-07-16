26303971406875e5c2173ac5c3507d894a89511f
Filipe <fstraubel10@gmail.com>
2025-07-16 15:21:58 -0300

    Feito estrutura base com pastas compartilhadas e arquivos genericos e inserido pacotes via nuget

---
+++ GeradorDeTestes.Infraestrutura.Orm/Compartilhado/GeradorDeTestesDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeradorDeTestes.Infraestrutura.Orm.Compartilhado
+{
+    public class GeradorDeTestesDbContext : DbContext 
+    {
+
+
+        public GeradorDeTestesDbContext(DbContextOptions options) :base(options){}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var assembly = typeof(GeradorDeTestesDbContext).Assembly;
+
+            modelBuilder.ApplyConfigurationsFromAssembly(assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ GeradorDeTestes.Infraestrutura.Orm/Compartilhado/RepositorioBaseEmOrm.cs
@@ -0,0 +1,54 @@
+﻿using GeradorDeTestes.Dominio.Compartilhado;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeradorDeTestes.Infraestrutura.Orm.Compartilhado
+{
+    public class RepositorioBaseEmOrm<T> where T : EntidadeBase<T>    
+    {
+        private readonly DbSet<T> registros;
+
+        public RepositorioBaseEmOrm(GeradorDeTestesDbContext contexto)
+        {
+            this.registros = contexto.Set<T>();
+        }
+
+        public void CadastrarRegistro(T novoRegistro)
+        {
+            registros.Add(novoRegistro);
+        }
+
+        public bool EditarRegistro(Guid idRegistro, T registroEditado)
+        {
+            var registroSelecionado = SelecionarRegistroPorId(idRegistro);
+
+            if (registroSelecionado is null)
+                return false;
+
+            registroSelecionado.AtualizarRegistro(registroEditado);
+
+            return true;
+        }
+
+        public bool ExcluirRegistro(Guid idRegistro)
+        {
+            var registroSelecionado = SelecionarRegistroPorId(idRegistro);
+
+            if (registroSelecionado is null)
+                return false;
+
+            registros.Remove(registroSelecionado);
+
+            return true;
+        }
+
+        public virtual T? SelecionarRegistroPorId(Guid idRegistro)
+        {
+            return registros.FirstOrDefault(x => x.Id.Equals(idRegistro));
+        }
+
+        public virtual List<T> SelecionarRegistros()
+        {
+            return registros.ToList();
+        }
+    }
+}
