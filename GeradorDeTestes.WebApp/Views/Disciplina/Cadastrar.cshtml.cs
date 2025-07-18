cc1581bb07064eb16da58cdfbfaed0c83019e699
Philipi Sauer <philipisauer09@gmail.com>
2025-07-18 15:35:34 -0300

    criado Views Disciplina

---
+++ GeradorDeTestes.WebApp/Views/Disciplina/Cadastrar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GeradorDeTestes.WebApp.Views.Disciplina
+{
+    public class CadastrarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
