b41fdb79881dc4e4b705bfa4262916a190ec44be
Philipi Sauer <philipisauer09@gmail.com>
2025-07-18 15:22:31 -0300

    criado Disciplina Extensios

---
+++ GeradorDeTestes.WebApp/Controllers/DisciplinaController.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GeradorDeTestes.WebApp.Controllers
+{
+    [Route("disciplina")]
+    public class DisciplinaController : Controller
+    {
+        private readonly ContextoDados contexto;
+        private readonly IRepositorioDisciplina repositorioDisciplina;
+
+        public DisciplinaController()
+        {
+            contexto = new ContextoDados(true);
+            repositorioDisciplina = new RepositorioDisciplina(contexto);
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var registros = repositorioDisciplina.SelecionarRegistros();
+
+            var visualizarVM = new VisualizarDisciplinasViewModel(registros);
+
+            return View(visualizarVM);
+        }
+
+        [HttpGet("cadastrar")]
+        public IActionResult Cadastrar()
+        {
+            var cadastrarVM = new CadastrarDisciplinaViewModel();
+
+            return View(cadastrarVM);
+        }
+
+        [HttpPost("cadastrar")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Cadastrar(CadastrarDisciplinaViewModel cadastrarVM)
+        {
+            var registros = repositorioDisciplina.SelecionarRegistros();
+
+            foreach (var item in registros)
+            {
+                if (item.Nome.Equals(cadastrarVM.Nome))
+                {
+                    ModelState.AddModelError("CadastroUnico", "Já existe uma Disciplina registrada com este título.");
+                    break;
+                }
+            }
+
+            if (!ModelState.IsValid)
+                return View(cadastrarVM);
+
+            var entidade = cadastrarVM.ParaEntidade();
+
+            repositorioDisciplina.CadastrarRegistro(entidade);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet("editar/{id:guid}")]
+        public ActionResult Editar(Guid id)
+        {
+            var registroSelecionado = repositorioDisciplina.SelecionarRegistroPorId(id);
+
+            var editarVM = new EditarDisciplinaViewModel(
+                id,
+                registroSelecionado.Nome
+            );
+
+            return View(editarVM);
+        }
+
+        [HttpPost("editar/{id:guid}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Editar(Guid id, EditarDisciplinaViewModel editarVM)
+        {
+            var registros = repositorioDisciplina.SelecionarRegistros();
+
+            foreach (var item in registros)
+            {
+                if (!item.Id.Equals(id) && item.Nome.Equals(editarVM.Nome))
+                {
+                    ModelState.AddModelError("CadastroUnico", "Já existe uma Disciplina registrada com este título.");
+                    break;
+                }
+            }
+
+            if (!ModelState.IsValid)
+                return View(editarVM);
+
+            var entidadeEditada = editarVM.ParaEntidade();
+
+            repositorioDisciplina.EditarRegistro(id, entidadeEditada);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet("excluir/{id:guid}")]
+        public IActionResult Excluir(Guid id)
+        {
+            var registroSelecionado = repositorioDisciplina.SelecionarRegistroPorId(id);
+
+            var excluirVM = new ExcluirDisciplinaViewModel(registroSelecionado.Id, registroSelecionado.Nome);
+
+            return View(excluirVM);
+        }
+
+        [HttpPost("excluir/{id:guid}")]
+        [ValidateAntiForgeryToken]
+        public IActionResult ExcluirConfirmado(Guid id)
+        {
+            repositorioDisciplina.ExcluirRegistro(id);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet("detalhes/{id:guid}")]
+        public IActionResult Detalhes(Guid id)
+        {
+            var registroSelecionado = repositorioDisciplina.SelecionarRegistroPorId(id);
+
+            var detalhesVM = new DetalhesDisciplinaViewModel(
+                id,
+                registroSelecionado.Nome,
+                registroSelecionado.Despesas
+            );
+
+            return View(detalhesVM);
+        }
+    }
+}
---
+++ GeradorDeTestes.WebApp/Extensions/DisciplinaExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace GeradorDeTestes.WebApp.Extensions
+{
+    public static class DisciplinaExtensions;
+
+    public static class DisciplinaExtensions
+    {
+        public static Disciplina ParaEntidade(this FormularioDisciplinaViewModel formularioVM)
+        {
+            return new Disciplina(formularioVM.Titulo);
+        }
+
+        public static DetalhesDisciplinaViewModel ParaDetalhesVM(this Disciplina categoria)
+        {
+            return new DetalhesDisciplinaViewModel(
+                    categoria.Id,
+                    categoria.Nome,
+                    categoria.Materia
+            );
+        }
+    }
+}
