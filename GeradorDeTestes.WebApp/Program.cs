95bd7749b8c1a9793120dc5251b4eae8ea45c73f
gkirschnxr <kirschnxr@gmail.com>
2025-07-20 14:27:54 -0300

    Adicionada Injeção de Dependencia e configurado Controllers na Program

--- GeradorDeTestes.WebApp/Program.cs
+++ GeradorDeTestes.WebApp/Program.cs
@@ -1,3 +1,7 @@
+using GeradorDeTestes.Dominio.ModuloMateria;
+using GeradorDeTestes.Infraestrutura.Orm.ModuloMateria;
+using GeradorDeTestes.WebApp.DependencyInjection;
+
 namespace GeradorDeTestes.WebApp
 {
     public class Program
@@ -5,9 +9,25 @@ namespace GeradorDeTestes.WebApp
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddScoped<IRepositorioMateria, RepositorioMateriaEmOrm>();
+
+            builder.Services.AddEntityFrameworkConfig(builder.Configuration);
+            builder.Services.AddSerilogConfig(builder.Logging);
+
+            builder.Services.AddControllersWithViews();
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            if (!app.Environment.IsDevelopment())
+                app.UseExceptionHandler("/erro");
+            else
+                app.UseDeveloperExceptionPage();
+
+            app.UseAntiforgery();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.MapDefaultControllerRoute();
 
             app.Run();
         }
